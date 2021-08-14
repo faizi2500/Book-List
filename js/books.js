30e63c93d9d34da1d0aaba55b84b2bfbeb2f55ea
Faizan <faizan.zahid2500@gmail.com>
2021-08-14 16:11:25 +0500

    Add local Storage functionality

--- js/books.js
+++ js/books.js
@@ -5,10 +5,6 @@ const btn = document.getElementById('addbtn');
 
 btn.addEventListener('click', input);
 
-//  cases for inputs :  1. Input fields empty => show error 2. Title and author should not match any previous book  3. Store value as objects and push them to the array books []
-
-
-
 function input(e) {
   e.preventDefault();
   const bookTitle =document.getElementById('title').value;  // bookTitle as input from book name
@@ -16,79 +12,53 @@ function input(e) {
   let status = true //this is to make sure that the book list is pushed only if it is true otherwise that line wont work.
   const error = document.createElement('p');
 
-  const list = {
-    id: '1',
-    title: '2',
-    author: '3',
-  };
-
   // check if the values are empty 
   if(bookTitle === "" || bookAuthor === "") {
     error.innerHTML = `<strong>Required:</strong> Please insert Book Name and Author Name`;
     body.appendChild(error);
-    // status = false;
+    status = false;
   }
-  else if(bookTitle !== "" || bookAuthor !== "") {    // checking if there is a similar book with the name and title. 
-    // for(let i = 0; i < books.length; i++) {
-      books.forEach((book)=> {
-        if(book.title == bookTitle && book.author == bookAuthor){
+  else  {    // checking if there is a similar book with the name and title. 
+    for(let i = 0; i < books.length; i++) {
+        if(books[i].title == bookTitle && books[i].author == bookAuthor){
           error.innerHTML = `<strong>Note: </strong> This book already exists in your Reading List.`
           body.appendChild(error);
-          // status = false;
+          status = false;
         }
-      })
+      }
   } 
-  else {
-    books.push(list);
-    list = {
+  if (status) {
+    const list = {
       id: Date(),
       title: `${bookTitle}`,
       author: `${bookAuthor}`,
     };
+    books.push(list);
+    updateStorage()
   }
-  
-  console.log(books);
-    
 
-// storelocal();
-form.reset();
+  console.log(books)
+  console.log(books.length)
 }
 
-// console.log(books)
- 
-function storelocal() {
-  const books = localStorage.getItem(JSON.strin)
+// Update Local Storage
+function updateStorage() {
+  localStorage.setItem('books',JSON.stringify(books));
 }
 
+// Parse data that is inside the storage so it is in the form of object not string. 
 
+const parseStorage = JSON.parse(localStorage.getItem('books'));
+if(parseStorage !== null) {
+  displayBooks();
+}
 
+let parent = document.createElement('ul')
 
-  // function input(e) {
-  //  e.preventDefault();
-  //  if(bookTitle == "" || bookAuthor == "") {
-  //    console.log('Please enter the book name and author name!!')
-  //  } else {
-  //      const list = {
-  //       id: Date(),
-  //       title: bookTitle,
-  //       author: bookAuthor,
-  //     }
-  //   }
-  // books.push(list);  
-  // storeList();
-  // console.log(books);
-  // }
-
-  // function storeList() {
-
-  // }
-
-
-
-
-
-
-
-
-
-
+function displayBooks() {
+  for(let i = 0; i < books.length; i++){
+     let li = document.createElement('li');
+     li.innerHTML = `<h3 class=book-heading> ${books[i].title} </h3>`;
+     parent.appendChild(li);
+  }
+}
